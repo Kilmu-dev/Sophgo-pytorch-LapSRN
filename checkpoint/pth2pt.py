d6c8906869d109e676d844f3dee4583a63d30818
Kilmu <fuchao.wang@sophgo.com>
2022-05-02 15:07:13 +0800

    更改加载模型方式

--- checkpoint/pth2pt.py
+++ checkpoint/pth2pt.py
@@ -2,8 +2,8 @@ import torch
 from lapsrn import Net
 
 model = Net()
-model = model.load_state_dict(torch.load('lapsrn_model_epoch_100.pth', map_location=torch.device('cpu')))#保存的训练模型
+model = torch.load('lapsrn_model_epoch_1.pth', map_location=torch.device('cpu')) #保存的训练模型
 model.eval()#切换到eval（）
-example = torch.rand(1, 3, 320, 480)#生成一个随机输入维度的输入
+example = torch.rand(64, 1, 3, 3)#生成一个随机输入维度的输入
 traced_script_module = torch.jit.trace(model, example)
-traced_script_module.save("LspSRN.pt")
+traced_script_module.save("LspSRN_epoch_1.pt")
