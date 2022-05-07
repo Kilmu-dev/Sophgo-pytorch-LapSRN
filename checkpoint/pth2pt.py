af843eecb8fdb6f4559d7c69393107c13bba5fb7
Kilmu <fuchao.wang@sophgo.com>
2022-05-07 14:01:00 +0800

    迭代80次生成pt

--- checkpoint/pth2pt.py
+++ checkpoint/pth2pt.py
@@ -2,8 +2,8 @@ import torch
 from lapsrn import Net
 
 model = Net()
-model = torch.load('lapsrn_model_epoch_1.pth', map_location=torch.device('cpu')) #保存的训练模型
+model = torch.load('lapsrn_model_epoch_80.pth', map_location=torch.device('cpu')) #保存的训练模型
 model.eval()#切换到eval（）
 example = torch.rand(64, 1, 3, 3)#生成一个随机输入维度的输入
 traced_script_module = torch.jit.trace(model, example)
-traced_script_module.save("LspSRN_epoch_1.pt")
+traced_script_module.save("LspSRN_epoch_80.pt")
